342a21729eefaaf65060a97ed6a87bea39afa034
= <=>
2023-12-25 17:04:55 +0000

    Return the ration of the two arguments, instead of always 1 in 'getRatio' function.

--- client3.py
+++ client3.py
@@ -43,7 +43,12 @@ def getDataPoint(quote):
 def getRatio(price_a, price_b):
     """ Get ratio of price_a and price_b """
     """ ------------- Update this function ------------- """
-    return 1
+    
+    if price_b == 0:
+        #avoid division by zero
+        return
+    
+    return price_a / price_b
 
 
 # Main
