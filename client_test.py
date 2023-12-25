b1afd54d448c3a6bbd2b2c0c8fabe5d6cf590beb
= <=>
2023-12-25 17:21:26 +0000

    Complete the tests for the 'getDataPoint' function

--- client_test.py
+++ client_test.py
@@ -16,9 +16,39 @@ class ClientTest(unittest.TestCase):
     ]
     """ ------------ Add the assertion below ------------ """
 
+class ClientTest(unittest.TestCase):
+    def test_getDataPoint_calculatePrice(self):
+      #test 'getDataPoint' function with normal quotes
+      quotes = [
+          {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453',
+              'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+          {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453',
+            'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+      ]
+      """ ------------ Add the assertion below ------------ """
+      for quote in quotes:
+        self.assertEqual(getDataPoint(quote), (quote['stock'], 
+                                                quote['top_bid']['price'], 
+                                                quote['top_ask']['price'], 
+                                                (quote['top_ask']['price'] + quote['top_bid']['price']) / 2)
+                        )
+          
 
-  """ ------------ Add more unit tests ------------ """
-
+    def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
+       #test 'getDataPoint' function when the bid price is greater than the ask price
+        quotes = [
+            {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453',
+                'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+            {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453',
+             'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+        ]
+        """ ------------ Add the assertion below ------------ """
+        for quote in quotes:
+          self.assertEqual(getDataPoint(quote), (quote['stock'], 
+                                                  quote['top_bid']['price'], 
+                                                  quote['top_ask']['price'], 
+                                                  (quote['top_ask']['price'] + quote['top_bid']['price']) / 2)
+                          )
 
 
 if __name__ == '__main__':
