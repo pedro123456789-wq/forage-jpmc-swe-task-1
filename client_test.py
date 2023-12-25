daa5811c6ab74cb74bef05ebbd45e3b423245ed2
= <=>
2023-12-25 17:22:37 +0000

    Create two tests for 'getRatio' function. The tests test normal usage and the boundary case where division by 0 would occur.

--- client_test.py
+++ client_test.py
@@ -1,20 +1,6 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint, getRatio
 
-class ClientTest(unittest.TestCase):
-  def test_getDataPoint_calculatePrice(self):
-    quotes = [
-      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
-      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
-    ]
-    """ ------------ Add the assertion below ------------ """
-
-  def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
-    quotes = [
-      {'top_ask': {'price': 119.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
-      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
-    ]
-    """ ------------ Add the assertion below ------------ """
 
 class ClientTest(unittest.TestCase):
     def test_getDataPoint_calculatePrice(self):
@@ -50,6 +36,15 @@ class ClientTest(unittest.TestCase):
                                                   (quote['top_ask']['price'] + quote['top_bid']['price']) / 2)
                           )
 
+    def test_getRatio_calculate(self):
+      #test 'getRatio' function with dummy data
+      self.assertEqual(getRatio(1, 2), 0.5)
+    
+    
+    def test_getRatio_handleInvalidInput(self):
+      #test 'getRatio' function when division by zero would occur
+      self.assertIsNone(getRatio(1, 0))
+      
 
 if __name__ == '__main__':
     unittest.main()
